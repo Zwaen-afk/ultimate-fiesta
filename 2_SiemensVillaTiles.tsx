c389005537ae36965634d6568bfb3bbca607341a
zwaehhhn <80837124+zwaehhhn@users.noreply.github.com>
2021-03-26 14:24:10 +0100

    Nuke

--- 2_SiemensVillaTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.02.15" name="2_SiemensVillaTiles" tilewidth="32" tileheight="32" tilecount="2500" columns="50">
- <image source="1_SiemensVillaTiles.png" width="1600" height="1600"/>
-</tileset>
